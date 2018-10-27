a3e6afd8c5b3a3b6dc494a3860c260fb4932c278
nsklaus <nsklaus@yandex.com>
2018-10-27 22:50:17 +0200

    fixing stuffs

--- pygo.py
+++ pygo.py
@@ -4,17 +4,12 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 import telnetlib
-
-import time
-from PIL import Image, ImageTk
-import os
 import re
 
 
 class App(object):
 
     def __init__(self):
-
         self.root = tkinter.Tk()
         self.canvas = Canvas(self.root, width=1120, height=540)
         self.canvas.grid()
@@ -51,63 +46,58 @@ class App(object):
         goban_width = (self.im_goban.width())
         goban_height = (self.im_goban.height())
         self.canvas.create_image(goban_width / 2, goban_height / 2, image=self.im_goban)
+        # self.canvas.pack()
 
         self.label_mouse = ttk.Label(self.root, text=str(self.site_nubr))
         self.label_mouse.place(x=50, y=goban_height + 20)
+        # self.label_mouse.pack()
 
         self.text1 = Text(self.root, height=30, width=80)
         self.text1.place(x=(goban_width + 5), y=0)
+        # self.text1.pack()
 
         self.entry_name = ttk.Entry(self.root, text=self.site_name, width=90)
         self.entry_name.place(x=470, y=goban_height + 35)
         self.entry_name.bind('<Return>', self.input_igs)
+        # self.entry_name.pack()
 
         button_enter = ttk.Button(self.root, text='ok')
         button_enter['command'] = self.input_igs
         button_enter.place(x=160, y=goban_height + 25)
+        # button_enter.pack()
 
         button_rem = ttk.Button(self.root, text='clean')
         button_rem['command'] = self.clean_board
         button_rem.place(x=260, y=goban_height + 25)
+        # button_rem.pack()
 
         button_key = ttk.Button(self.root, text='key')
         button_key['command'] = self.get_key
         button_key.place(x=360, y=goban_height + 25)
+        # button_key.pack()
         self.input_igs()
 
     def read_igs(self):
         my_buffer = self.tn.read_very_eager().decode('ascii')
-        if len(my_buffer) > 0:
-            print(self.text1.insert(INSERT, my_buffer))
+        if not my_buffer:
+            pass
+        else:
+            self.text1.insert(INSERT, my_buffer)
             self.text1.see("end")
 
             if self.observe:
-                # my_buffer = self.tn.read_very_eager().decode('ascii')
-                # print(re.findall(r'(?<=\(B\):)....', my_buffer))
-                # print(re.findall(r'(?<=\([B-W]\):)..\d+', my_buffer))  # find coords
-                # print(re.findall(r'([B\-W])', my_buffer))  # find color
-                # self.my_color = "B"
-                # self.my_coords.clear()
-                # self.my_coords = "J16"
+                # TODO: make it a true list, not fixed to [0], then loop on the list to treat captures
                 my_coords = re.findall(r'(?<=\([B-W]\):.).\d+', my_buffer)  # find coords
                 my_color = re.findall(r'([BW])', my_buffer)  # find color
-                # print("my_coords = ", my_coords[0], "\n")
-                # print("my_color = ", my_color[0], "\n")
-                # print("my_buffer = ", my_buffer, "\n")
-                # self.my_color = re.search(r'([B\-W])', my_buffer).group(0)
-                # print("mycolor = ", self.my_color, "\n")
-                # print("my_coords= ", self.my_coords, "\n")
-                # if "(" + self.my_color[0] + "):" in my_buffer:
-                    # stringafterword = re.match("(?<=\(B\): )...", my_buffer)
-                    # if stringafterword:
-                    #     print(self.text1.insert(INSERT, stringafterword + "\n"))
-                self.put_stone(my_color[0], my_coords[0])
+                if not my_color or not my_coords:
+                    pass
+                else:
+                    self.put_stone(my_color[0], my_coords[0])
         self.root.after(2000, self.read_igs)
 
     def input_igs(self, event=None):
         self.tn.write(self.entry_name.get().encode('ascii') + b"\n")
         if "observe" in self.entry_name.get():
-            print(self.text1.insert(INSERT, "====  putting in observe mode  ===="))
             self.observe = True
         self.text1.see("end")
         self.entry_name.delete(0, 'end')
@@ -120,12 +110,12 @@ class App(object):
 
     def clean_board(self):
         for stone in list(self.mydict.keys()):
-            get_letter = (re.findall(r'([A-Z])', stone )[-1])
-            get_pos_y = (re.findall(r'(\d+)', stone )[-1])
+            get_letter = (re.findall(r'([A-Z])', stone)[-1])
+            get_pos_y = (re.findall(r'(\d+)', stone)[-1])
             mykey = get_letter + get_pos_y
-            print("result2=", self.mydict[mykey])
             self.canvas.delete(self.mydict[mykey])
             del self.mydict[mykey]
+        self.mydict.clear()
 
     def put_stone(self, color, coords):
         # move_coords = self.entry_name.get()
@@ -133,11 +123,7 @@ class App(object):
         get_pos_y = (re.findall(r'(\d+)', coords)[-1])
         mykey = get_letter + get_pos_y
         res_x = int(self.letter_list.index(get_letter)) * 23 + 32
-        print("coords = ", coords, " get_pos_y = ", get_pos_y, "\n")
-        print((32 + (int(get_pos_y) * 23)))
         res_y = 32 + ((19 - int(get_pos_y)) * 23)   # (32 + (int(get_pos_y) * 23))
-        # print("res_y = ", res_y, "\n")
-        # print(self.text1.insert(INSERT, "(" + color + ")" + coords + "\n"))
         if mykey in self.mydict:
             print("already exists!")
         else:
@@ -160,7 +146,7 @@ class App(object):
             self.last_y = res_y
 
     def mouse_click(self, event):
-        print("clicked --> pixel=[{}] go=[{}]".format(str(event.x) + "x" + str(event.y), self.last_x + self.last_y))
+        # print("clicked --> pixel=[{}] go=[{}]".format(str(event.x) + "x" + str(event.y), self.last_x + self.last_y))
         get_pos_x = self.letter_list.index(self.last_x)
         get_pos_y = self.last_y
         rel_pos_x = 33 + (int(get_pos_x) * 23)
