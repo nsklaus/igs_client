2a80413dfde30892470bd3acb9c41d9a4425b64c
nsklaus <nsklaus@yandex.com>
2018-10-28 22:48:14 +0100

    making things do stuffs

--- pygo.py
+++ pygo.py
@@ -4,6 +4,7 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 import telnetlib
+import time
 import re
 
 
@@ -11,7 +12,10 @@ class App(object):
 
     def __init__(self):
         self.root = tkinter.Tk()
-        self.canvas = Canvas(self.root, width=1120, height=540)
+        self.frame_main = Frame(self.root, bg="gray")
+        # self.frame_main.pack()
+        self.canvas = Canvas(self.root, width=1100, height=540)
+
         self.canvas.grid()
         self.canvas.bind("<Button-1>", self.mouse_click)
         self.canvas.bind('<Motion>', self.mouse_motion)
@@ -20,7 +24,7 @@ class App(object):
         self.root.title('go client')
         self.HOST = "igs.joyjoy.net"
         self.PORT = 7777
-        self.tn = telnetlib.Telnet(self.HOST, self.PORT)
+        self.tn = telnetlib.Telnet()
 
         self.last_x = 0
         self.last_y = 0
@@ -30,6 +34,7 @@ class App(object):
         self.my_color = []
         self.observe = False
         self.turn = True
+        self.status = "Offline"
 
         self.site_name = StringVar()
         self.site_nubr = 0
@@ -46,67 +51,102 @@ class App(object):
         goban_width = (self.im_goban.width())
         goban_height = (self.im_goban.height())
         self.canvas.create_image(goban_width / 2, goban_height / 2, image=self.im_goban)
-        # self.canvas.pack()
 
         self.label_mouse = ttk.Label(self.root, text=str(self.site_nubr))
-        self.label_mouse.place(x=50, y=goban_height + 20)
-        # self.label_mouse.pack()
+        self.label_mouse.place(x=10, y=goban_height + 20)
 
         self.text1 = Text(self.root, height=30, width=80)
         self.text1.place(x=(goban_width + 5), y=0)
-        # self.text1.pack()
+        self.vsb = ttk.Scrollbar(self.root, orient="vertical", command=self.text1.yview)
+        self.vsb.grid(column=1, row=0, sticky='ns')
+        self.text1.configure(yscrollcommand=self.vsb.set)
+
+        # self.textframe = Text(self.root, bd=2, bg='#CEF6EC', relief=RAISED)
+        # self.txscroll = ttk.Scrollbar(self.root, orient=VERTICAL, command=self.textframe.yview)
+        # self.txscroll.grid(row=0, column=1, sticky='ns')
+        # self.textframe.configure(yscroll=self.txscroll.set)
+        # self.textframe.grid(row=0, column=0, padx=2, pady=2)
 
         self.entry_name = ttk.Entry(self.root, text=self.site_name, width=90)
         self.entry_name.place(x=470, y=goban_height + 35)
         self.entry_name.bind('<Return>', self.input_igs)
-        # self.entry_name.pack()
 
-        button_enter = ttk.Button(self.root, text='ok')
+        self.button_enter = ttk.Button(self.root, text='Online')
+        self.button_enter['command'] = self.toggle_net
+        self.button_enter.place(x=60, y=goban_height + 25)
+
+        button_enter = ttk.Button(self.root, text='disconnect')
         button_enter['command'] = self.input_igs
         button_enter.place(x=160, y=goban_height + 25)
-        # button_enter.pack()
 
         button_rem = ttk.Button(self.root, text='clean')
         button_rem['command'] = self.clean_board
         button_rem.place(x=260, y=goban_height + 25)
-        # button_rem.pack()
 
         button_key = ttk.Button(self.root, text='key')
         button_key['command'] = self.get_key
         button_key.place(x=360, y=goban_height + 25)
-        # button_key.pack()
+
+    def toggle_net(self):
+        if self.button_enter["text"] == "Online":
+            self.button_enter["text"] = "Offline"
+            self.go_online()
+        elif self.button_enter["text"] == "Offline":
+            self.button_enter["text"] = "Online"
+            self.go_offline()
+
+    def go_online(self):
+        self.status = "Online"
+        self.text1.insert(INSERT, "\n" + " === Connecting to IGS ===" + "\n\n")
+        self.tn = telnetlib.Telnet(self.HOST, self.PORT)
+        self.read_igs()
         self.input_igs()
 
-    def read_igs(self):
-        my_buffer = self.tn.read_very_eager().decode('ascii')
-        if not my_buffer:
-            pass
-        else:
-            self.text1.insert(INSERT, my_buffer)
-            self.text1.see("end")
+    def go_offline(self):
+        self.status = "Offline"
+        try:
+            self.tn.write(b"exit" + b"\n")
+            time.sleep(1)
+            self.text1.insert(INSERT, "\n\n" + " === IGS connection terminated ===" + "\n")
+        except EOFError as e:
+            self.text1.insert(INSERT, str(e))
+        self.text1.see("end")
 
-            if self.observe:
-                # TODO: make it a true list, not fixed to [0], then loop on the list to treat captures
-                my_coords = re.findall(r'(?<=\([B-W]\):.).\d+', my_buffer)  # find coords
-                my_color = re.findall(r'([BW])', my_buffer)  # find color
-                if not my_color or not my_coords:
-                    pass
-                else:
-                    self.put_stone(my_color[0], my_coords[0])
-        self.root.after(2000, self.read_igs)
+    def read_igs(self):
+        if self.status == "Online":
+            my_buffer = self.tn.read_very_eager().decode('ascii')
+            if not my_buffer:
+                pass
+            else:
+                self.text1.insert(INSERT, my_buffer)
+                self.text1.see("end")
+
+                if self.observe:
+                    # TODO: when starting to observe, loop first through previous moves (moves <game_id>)
+                    # TODO: make it a true list, not fixed to [0], then loop on the list to treat captures
+                    # TODO: investigate some moves are listed from buffer but not replicated on the UI (bad regex?)
+                    my_coords = re.findall(r'(?<=\([B-W]\):.).\d+', my_buffer)  # find coords
+                    my_color = re.findall(r'([BW])', my_buffer)  # find color
+                    if not my_color or not my_coords:
+                        pass
+                    else:
+                        print("my_buffer=[", my_buffer, "]\n")
+                        self.put_stone(my_color[0], my_coords[0])
+            self.root.after(2000, self.read_igs)
 
     def input_igs(self, event=None):
-        self.tn.write(self.entry_name.get().encode('ascii') + b"\n")
-        if "observe" in self.entry_name.get():
-            self.observe = True
-        self.text1.see("end")
-        self.entry_name.delete(0, 'end')
+        if self.status == "Online":
+            self.tn.write(self.entry_name.get().encode('ascii') + b"\n")
+            if "observe" in self.entry_name.get():
+                self.observe = True
+            self.text1.see("end")
+            self.entry_name.delete(0, 'end')
 
     def get_key(self):
         print("mydict_len=", len(self.mydict))
         print("mydict=", self.mydict)
         for key in self.mydict:
-            print("key=", key, "stuff=", self.mydict[key])
+            print("key=", key, "stuff=", self.mydict[key], "type= ", type(self.mydict[key]))
 
     def clean_board(self):
         for stone in list(self.mydict.keys()):
@@ -117,15 +157,20 @@ class App(object):
             del self.mydict[mykey]
         self.mydict.clear()
 
+    def del_stone(self, coords):
+        for stone in coords:
+            self.canvas.delete(self.mydict[stone])
+            del self.mydict[stone]
+
     def put_stone(self, color, coords):
-        # move_coords = self.entry_name.get()
         get_letter = (re.findall(r'([A-Z])', coords)[-1])
         get_pos_y = (re.findall(r'(\d+)', coords)[-1])
         mykey = get_letter + get_pos_y
-        res_x = int(self.letter_list.index(get_letter)) * 23 + 32
-        res_y = 32 + ((19 - int(get_pos_y)) * 23)   # (32 + (int(get_pos_y) * 23))
+        res_x = int(self.letter_list.index(get_letter)) * 23 + 32  # letter_index * offset + bordersize
+        res_y = 32 + ((19 - int(get_pos_y)) * 23)  # bordersize + (boardsize - letter) * offset
         if mykey in self.mydict:
             print("already exists!")
+            # self.del_stone(mykey)
         else:
             if color is 'B':
                 self.mydict[mykey] = self.canvas.create_image(res_x, res_y, image=self.im_black)
@@ -146,21 +191,21 @@ class App(object):
             self.last_y = res_y
 
     def mouse_click(self, event):
-        # print("clicked --> pixel=[{}] go=[{}]".format(str(event.x) + "x" + str(event.y), self.last_x + self.last_y))
         get_pos_x = self.letter_list.index(self.last_x)
         get_pos_y = self.last_y
         rel_pos_x = 33 + (int(get_pos_x) * 23)
         rel_pos_y = 469 - (int(get_pos_y) * 23)
         mykey = self.last_x + self.last_y
 
-        self.turn = not self.turn
         if mykey in self.mydict:
-            print("already exists!")
+            temp_list = [mykey]
+            self.del_stone(temp_list)
         else:
             if self.turn:
                 self.mydict[mykey] = self.canvas.create_image(rel_pos_x, rel_pos_y, image=self.im_black)
             else:
                 self.mydict[mykey] = self.canvas.create_image(rel_pos_x, rel_pos_y, image=self.im_white)
+            self.turn = not self.turn
 
 
 app = App()
